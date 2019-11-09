dbbd1badb1b64ee0ec187b9f9db0d3405143561f
Brice Figureau <brice@daysofwonder.com>
2019-11-09 19:29:00 +0100

    Fix my E6.5 keymap
    * better handling of Grace/Esc on mac
    * missing function layer
    * DEL in R0
    * END in R4

--- keyboards/exclusive/e65/keymaps/masterzen/keymap.c
+++ keyboards/exclusive/e65/keymaps/masterzen/keymap.c
@@ -3,14 +3,15 @@
 enum layers {
     BASE,  // qwerty linux
     OSX,   // qwerty osx
-
-    _ADJUST,  // function key layer
+    _FL, // function key layer
+    _ADJUST,
 };
 
 enum keycodes {
     // default layout switcher
     LAY_LIN = SAFE_RANGE,
-    LAY_OSX
+    LAY_OSX,
+    TICK_ESC,
 };
 
 // clang-format off
@@ -32,12 +33,12 @@ enum keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT_65_ansi_blocker_splitbs(
-        KC_ESC, KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, LT(_ADJUST,KC_INS),
+        TICK_ESC, KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_DEL, LT(_ADJUST,KC_INS),
         KC_TAB, KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,
 
-        KC_LCTL, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,        KC_ENT, KC_PGDN,
-        KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT,KC_SLSH,         KC_RSFT, KC_UP, KC_DEL,
-        KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC,                      KC_RALT, KC_RCTRL, KC_LEFT, KC_DOWN,KC_RGHT),
+        LCTL_T(KC_ESC), KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,        KC_ENT, KC_PGDN,
+        KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT,KC_SLSH,         KC_RSFT, KC_UP, KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,                 LT(_FL,KC_SPC),                      KC_RALT, KC_RCTRL, KC_LEFT, KC_DOWN,KC_RGHT),
 
     [OSX] = LAYOUT_65_ansi_blocker_splitbs(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -45,6 +46,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_LCTL, KC_LALT, KC_LGUI,                  _______,                             KC_LGUI, KC_RALT, _______, _______, _______),
+
+    [_FL] = LAYOUT_65_ansi_blocker_splitbs(
+        KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12, KC_F13 , KC_DEL, MO(_ADJUST),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,_______,
+        _______, _______, _______,                  _______,                             _______,   _______, _______, _______, _______),
 
     [_ADJUST] = LAYOUT_65_ansi_blocker_splitbs(
         RESET,   LAY_LIN, LAY_OSX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MO(_ADJUST),
@@ -57,6 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // clang-format on
+static bool tick_esc_was_shifted = false;
 
 extern rgblight_config_t rgblight_config;
 
@@ -90,6 +99,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
+        case TICK_ESC: {
+            uint8_t guied = get_mods() & (MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
+
+            if (record->event.pressed) {
+                tick_esc_was_shifted = guied;
+                add_key(guied ? KC_ESCAPE : KC_GRAVE);
+            } else {
+                del_key(tick_esc_was_shifted ? KC_ESCAPE : KC_GRAVE);
+            }
+
+            send_keyboard_report();
+            return false;
+        }
     }
     return true;
 }
