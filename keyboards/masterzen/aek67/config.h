1488442f35890e250f70212be2c5add7c8648108
Brice Figureau <brice@daysofwonder.com>
2020-02-09 19:28:43 +0100

    Add the AEK67 project

---
+++ keyboards/masterzen/aek67/config.h
@@ -0,0 +1,55 @@
+/* Copyright 2019 Brice Figureau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x6767
+#define DEVICE_VER   0x0001
+#define MANUFACTURER masterzen
+#define PRODUCT      AEK67
+#define DESCRIPTION  AEK67 QMK PCB
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS \
+    { B0, B1, F5, F4, F1 }
+#define MATRIX_COL_PINS \
+    { D0, D1, D2, D3, D5, D4, D6, D7, B4, B5, B6, C6, C7, F7, F6 }
+#define UNUSED_PINS \
+    { B7, E6 }
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+#undef BACKLIGHT_PIN
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#undef RGB_DI_PIN
