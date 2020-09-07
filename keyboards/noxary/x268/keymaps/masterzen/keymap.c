05a9b32dc882dcc940de88c37e34427e7fb2dd79
Brice Figureau <brice@daysofwonder.com>
2020-09-07 14:17:27 +0200

    My lz-cls-ms keymap

---
+++ keyboards/noxary/x268/keymaps/masterzen/keymap.c
@@ -0,0 +1,151 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+    BASE,  // qwerty linux
+    OSX,   // qwerty osx
+    _FL, // function key layer
+    _ADJUST,
+};
+
+enum keycodes {
+    // default layout switcher
+    LAY_LIN = SAFE_RANGE,
+    LAY_OSX,
+    TICK_ESC,
+};
+
+// clang-format off
+
+/*
+ * ,---------------------------------------------------------------------|
+ * |`  |1  |2  |3  |4  |5  |6  |7  |8  |9  |0   |-   |=   |\  |Esc | Ins |
+ * |---------------------------------------------------------------------|
+ * |Tab  |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  | ]  |    BSpc| PgUp|
+ * |---------------------------------------------------------------------|
+ * |LCTRL |A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  |  Enter    | PgDn |
+ * |---------------------------------------------------------------------|
+ * |Shft    |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/   |   Shift | Up |Del |
+ * |---------------------------------------------------------------------|
+ * |Ctrl|GUI |Alt |     Space                    |Alt| Ctrl|▓|Lt |Dn |Rt |
+ * `---------------------------------------------------------------------|'
+ */
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [BASE] = LAYOUT(
+        TICK_ESC, KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, LT(_ADJUST,KC_HOME),
+        KC_TAB, KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,
+
+        LCTL_T(KC_ESC), KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,        _______, KC_ENT, KC_PGUP,
+        KC_LSFT, _______, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT,KC_SLSH,         KC_RSFT, KC_UP, KC_PGDN,
+        KC_LCTL, KC_LGUI, KC_LALT,                 LT(_FL,KC_SPC),                      KC_RALT, KC_RCTRL, _______, KC_LEFT, KC_DOWN,KC_RGHT),
+
+    [OSX] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_LCTL, KC_LALT, KC_LGUI,                  _______,                             KC_LGUI, KC_RALT, _______,_______, _______, _______),
+
+    [_FL] = LAYOUT(
+        KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12, KC_F13 , KC_DEL, MO(_ADJUST),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,_______,
+        _______, _______, _______,                  _______,                             _______,   _______, _______, _______, _______,_______),
+
+    [_ADJUST] = LAYOUT(
+        RESET,   LAY_LIN, LAY_OSX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MO(_ADJUST),
+        BL_TOGG, BL_DEC, BL_INC, KC_LGUI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, _______,
+        _______, _______, _______,                  _______,                             _______, _______,_______, RGB_MOD, RGB_VAD, RGB_HUD),
+
+
+};
+
+// clang-format on
+static bool tick_esc_was_shifted = false;
+
+extern rgblight_config_t rgblight_config;
+
+bool     edit = false;
+rgblight_config_t temp_config;
+
+void keyboard_post_init_user(void) { temp_config.raw = rgblight_config.raw; }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        /* layout switcher */
+        case LAY_LIN:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(BASE);
+            }
+            return false;
+            break;
+        case LAY_OSX:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(OSX);
+            }
+            return false;
+            break;
+        case BL_TOGG: {
+            if (record->event.pressed) {
+                if (!is_backlight_enabled()) {
+                    writePinLow(B6);
+                } else {
+                    writePinHigh(B6);
+                }
+                return false;
+            }
+            break;
+        }
+        case RGB_MOD:
+            // allows to set the rgb mode while in the ADJUST layer which uses
+            // its own mode
+            if (record->event.pressed) {
+                rgblight_mode(temp_config.mode);
+                rgblight_step();
+                temp_config.mode = rgblight_config.mode;
+            }
+            return false;
+            break;
+        case TICK_ESC: {
+            uint8_t guied = get_mods() & (MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
+
+            if (record->event.pressed) {
+                tick_esc_was_shifted = guied;
+                add_key(guied ? KC_ESCAPE : KC_GRAVE);
+            } else {
+                del_key(tick_esc_was_shifted ? KC_ESCAPE : KC_GRAVE);
+            }
+
+            send_keyboard_report();
+            return false;
+        }
+    }
+    return true;
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+    if (state == BASE && edit == true) {
+        temp_config.hue  = rgblight_get_hue();
+        temp_config.sat  = rgblight_get_sat();
+        temp_config.val  = rgblight_get_val();
+        edit = false;
+    }
+
+    switch (biton32(state)) {
+        case _ADJUST:
+            temp_config.mode = rgblight_get_mode();
+            rgblight_mode_noeeprom(1);
+            rgblight_setrgb(0xD3, 0x7F, 0xED);
+            edit = true;
+            break;
+        default:
+            rgblight_mode(temp_config.mode);
+            rgblight_sethsv(temp_config.hue, temp_config.sat, temp_config.val);
+            break;
+    }
+    return state;
+}
